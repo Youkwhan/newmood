c02447ee3ad9663306f6248138f4aa7f63cc3fee
Youkwhan <Youkwhan@gmail.com>
2023-12-13 22:49:15 -0500

    feat(Client): App.jsx:Implemented scroll-to-top behavior on route changes
    Integrated react-router v6 in App.jsx and added ScrollToTop component to reset the scroll position to the top of the page when navigating between routes.
    
    - <ScrollToTop /> component is now part of the main application's render cycle and re-render whenever a route changes.
    - This ensures it will be mounted and unmounted with each route change, ensuring that the scroll behavior is consistent across all routes.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,33 @@
+import { useEffect } from "react"
+import {
+	createBrowserRouter,
+	RouterProvider,
+	createRoutesFromElements,
+	Route,
+	useLocation,
+} from "react-router-dom"
+
+// When redirected to a page, start at the top of the page.
+const ScrollToTop = () => {
+	const { pathname } = useLocation()
+
+	useEffect(() => {
+		window.scrollTo(0, 0)
+	}, [pathname])
+	return null
+}
+
+const router = createBrowserRouter(
+	createRoutesFromElements(<Route path="/" element={<Home />} />)
+)
+
 function App() {
-	return <div className="app">Hello</div>
+	return (
+		<div className="app">
+			<ScrollToTop />
+			<RouterProvider router={router} />
+		</div>
+	)
 }
 
 export default App
