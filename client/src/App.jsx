6339aa19f25fd9ab5ad4e450467eee47437d6d48
Youkwhan <Youkwhan@gmail.com>
2023-12-15 16:51:41 -0500

    feat(Client): Configured application routes and components
    - Added routes and corresponding components for the Home, ItemDetails, Checkout, and Confirmation scenes.
    - Implemented scrollToTop functionality to ensure each page renders from the top on navigation.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,10 @@ import {
 	Route,
 	useLocation,
 } from "react-router-dom"
+import Home from "./scenes/Home/Home"
+import ItemDetails from "./scenes/itemsDetails/ItemDetails"
+import Checkout from "./scenes/checkout/Checkout"
+import Confirmation from "./scenes/checkout/Confirmation"
 
 // When redirected to a page, start at the top of the page.
 const ScrollToTop = () => {
@@ -18,7 +22,13 @@ const ScrollToTop = () => {
 }
 
 const router = createBrowserRouter(
-	createRoutesFromElements(<Route path="/" element={<Home />} />)
+	createRoutesFromElements(
+		<Route path="/" element={<Home />}>
+			<Route path="item/:itemId" element={<ItemDetails />} />
+			<Route path="checkout" element={<Checkout />} />
+			<Route path="checkout/success" element={<Confirmation />} />
+		</Route>
+	)
 )
 
 function App() {
