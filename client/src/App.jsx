cc6b702d512aadb4665d813ba1582d87f1f61bec
Youkwhan <Youkwhan@gmail.com>
2023-12-17 21:01:50 -0500

    feat(Client): Created Navbar component with Material UI integration
    Implemented Navbar component using Material UI, integrated Redux dispatch functionality, and set up navigation using react-router-dom's useNavigate hook.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,10 @@
 import { useEffect } from "react"
-import {
-	createBrowserRouter,
-	RouterProvider,
-	createRoutesFromElements,
-	Route,
-	useLocation,
-} from "react-router-dom"
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom"
 import Home from "./scenes/Home/Home"
 import ItemDetails from "./scenes/itemsDetails/ItemDetails"
 import Checkout from "./scenes/checkout/Checkout"
 import Confirmation from "./scenes/checkout/Confirmation"
-
+import Navbar from "./scenes/global/Navbar"
 // When redirected to a page, start at the top of the page.
 const ScrollToTop = () => {
 	const { pathname } = useLocation()
@@ -21,21 +15,19 @@ const ScrollToTop = () => {
 	return null
 }
 
-const router = createBrowserRouter(
-	createRoutesFromElements(
-		<Route path="/" element={<Home />}>
-			<Route path="item/:itemId" element={<ItemDetails />} />
-			<Route path="checkout" element={<Checkout />} />
-			<Route path="checkout/success" element={<Confirmation />} />
-		</Route>
-	)
-)
-
 function App() {
 	return (
 		<div className="app">
-			<ScrollToTop />
-			<RouterProvider router={router} />
+			<BrowserRouter>
+				<Navbar />
+				<ScrollToTop />
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="item/:itemId" element={<ItemDetails />} />
+					<Route path="checkout" element={<Checkout />} />
+					<Route path="checkout/success" element={<Confirmation />} />
+				</Routes>
+			</BrowserRouter>
 		</div>
 	)
 }
